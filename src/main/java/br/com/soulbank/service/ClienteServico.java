f1513d51c67f9788fcf65bb64d3820d078f5f8c9
joycebarbosag <joycebarbosa.gomess@gmail.com>
2021-07-23 15:08:39 -0300

    Projeto com classes controller e service cliente comentadas

--- src/main/java/br/com/soulbank/service/ClienteServico.java
+++ src/main/java/br/com/soulbank/service/ClienteServico.java
@@ -5,37 +5,47 @@ import br.com.soulbank.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-
+/*
+ * INDICA QUE É UMA CLASSE DE SERVIÇO.
+ * 
+ * ISSO QUER DIZER QUE TODOS OS MÉTODOS DA CLASSE CLIENTE FICAM AQUI PARA SEREM CHAMADOS PELA CLASSE 'ClienteController'
+ */
 @Service
 public class ClienteServico {
-	
+
+	// INJEÇÃO DE DEPEDÊNCIA(DE UMA CLASSE-REPOSITORY- EM OUTRA-SERVICE- PORQUE A
+	// ÚLTIMA DEPENDE DA PRIMEIRA)
 	@Autowired
 	private ClienteRepository clientrepository;
 
+	// ESTE MÉTODO ESTÁ RETORNANDO PELO findALl UMA LISTA COMPLETA DA CLASSE
+	// CLIENTE.
 	public List<Cliente> findAll() {
 		return clientrepository.findAll();
 	}
 
+	/*
+	 * ESTE MÉTODO ESTÁ RETORNANDO UM CLIENTE PELO ID(getById).
+	 * 
+	 * EXPRESSÃO LAMBDA: PELO findById TENTA-SE ENCONTRAR O CLIENTE PELO ID, 'CASO
+	 * NÃO, LANÇA'(orElseThrow) INSTANCIA UMA NOVA ENTIDADE NO
+	 * 'EntityNotFoundException'.
+	 */
 	public Cliente getById(Long id) {
-		return clientrepository.findById(id)
-				.orElseThrow(() -> new EntityNotFoundException());
+		return clientrepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
 	}
 
+	// PELO MÉTODO, UM OBJETO CLIENTE É SALVO NA CLASSE CLIENTE.
 	public Cliente save(Cliente cliente) {
 		return clientrepository.save(cliente);
 	}
 
+	// PELO MÉTODO, UM OBJETO CLIENTE É DELETADO PELO ID NA CLASSE CLIENTE.
 	public void delete(Long id) {
 		clientrepository.deleteById(id);
 	}
-	
-	
-	
-	
-	
 
 }
