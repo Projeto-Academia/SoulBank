7638e1b179adb0dee12e95ef5c1c6dbffe73c31a
Flavia <flaviarenata969@gmail.com>
2021-07-24 19:01:53 -0300

    Adicionando HTML e CSS junto da Controller do extrato

--- src/main/java/br/com/soulbank/controller/ExtratoController.java
+++ src/main/java/br/com/soulbank/controller/ExtratoController.java
@@ -1,10 +1,30 @@
 package br.com.soulbank.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import br.com.soulbank.entity.Extrato;
+import br.com.soulbank.repository.ExtratoRepository;
 
 @RestController
-@RequestMapping
+@RequestMapping("/extrato")
 public class ExtratoController {
+	
+	@Autowired
+	private ExtratoRepository extrato;
+		
+		@GetMapping
+		public ModelAndView listar() {
+			List<Extrato> listaextrato= extrato.findAll();
+			
+			ModelAndView modelAndView = new ModelAndView("extrato");		
+			modelAndView.addObject("extrato", listaextrato);
+			
+			return modelAndView;
+		}
+	}
 
-}
