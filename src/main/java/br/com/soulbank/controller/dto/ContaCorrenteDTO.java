a4bd811f291833a8b07a5e4563674fb6672cba02
francielydbastos <francielydbastos@gmail.com>
2021-07-24 15:46:48 -0300

    Corrigindo ContaDTO

--- src/main/java/br/com/soulbank/controller/dto/ContaCorrenteDTO.java
+++ src/main/java/br/com/soulbank/controller/dto/ContaCorrenteDTO.java
@@ -1,14 +1,17 @@
 package br.com.soulbank.controller.dto;
 
-
-
 import javax.validation.constraints.NotNull;
-
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-<<<<<<< HEAD:src/main/java/br/com/soulbank/controller/dto/ContaDTO.java
-public class ContaDTO {
+public class ContaCorrenteDTO {
+
+
+	private long idContaCorrente;
+	
+	@NotNull
+	private double saldo;
 	
 	@NotNull
 	@Size(max = 5)
@@ -17,15 +20,4 @@ public class ContaDTO {
 	@NotNull
 	@Size(max = 5)
 	private Long idCliente;
-=======
-public class ContaCorrenteDTO {
-
-
-	private long idContaCorrente;
->>>>>>> 1cf7a5c20c5daabf476ff45f6f11eb9e1fd36e42:src/main/java/br/com/soulbank/controller/dto/ContaCorrenteDTO.java
-	
-	@NotNull
-	private double saldo;
-	
-	
 }
