aa85f0d9a87830adcef86adf866a7cadf78321b6
Adriel Muniz <adriel@gmx.com>
2021-07-26 18:07:59 -0300

    Formulario cadastro

--- src/main/java/br/com/soulbank/SoulbankApplication.java
+++ src/main/java/br/com/soulbank/SoulbankApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages = {"br.com.soulbank.entity"})
+@ComponentScan(basePackages = {"br.com.soulbank.*"})
+@EnableJpaRepositories(basePackages = {"br.com.soulbank.repository.*"})
+@EnableTransactionManagement
 public class SoulbankApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/soulbank/repository/TelaCadastroRepository.java
+++ src/main/java/br/com/soulbank/repository/TelaCadastroRepository.java
@@ -1,11 +1,14 @@
 package br.com.soulbank.repository;
 
+import br.com.soulbank.controller.dto.ClienteDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import br.com.soulbank.entity.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-
-public interface TelaCadastroRepository extends JpaRepository<Cliente, Long> {
+@Transactional
+public interface TelaCadastroRepository extends CrudRepository<ClienteDTO, Long> {
 
 }
