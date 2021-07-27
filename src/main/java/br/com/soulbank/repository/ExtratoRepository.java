f52e2d01a1340d05638b78664b57abf246d22bc6
francielydbastos <francielydbastos@gmail.com>
2021-07-27 16:04:09 -0300

    Filtrando dados do extrato para mostrar apenas o extrato de uma conta específica

--- src/main/java/br/com/soulbank/repository/ExtratoRepository.java
+++ src/main/java/br/com/soulbank/repository/ExtratoRepository.java
@@ -1,10 +1,8 @@
 package br.com.soulbank.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import br.com.soulbank.entity.Extrato;
 
 public interface ExtratoRepository extends JpaRepository<Extrato, Long> {
